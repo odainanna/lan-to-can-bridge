8de6822c97f4734771d5170ade6de60d090ace14
Oda Inanna Klemetsdal Stene <odainanna@gmail.com>
2023-04-17 23:05:34 +0200

    interface pcan

--- bridgeapp.py
+++ bridgeapp.py
@@ -9,7 +9,7 @@ parser.add_argument('--net', default='127.0.0.1')
 parser.add_argument('--port', default=62222, type=int)
 parser.add_argument('--seg', default=5, type=int)
 parser.add_argument('--bitrate', default=125000, type=int)
-parser.add_argument('--interface', default='virtual')
+parser.add_argument('--interface', default='pcan')
 args = parser.parse_args()
 
 
